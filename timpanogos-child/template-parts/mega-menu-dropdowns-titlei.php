8c4eaa44fe0d43ec68903489982d1dece66579a2
Espi <joshespi@gmail.com>
2021-12-03 15:57:38 -0700

    initial upload

---
+++ timpanogos-child/template-parts/mega-menu-dropdowns-titlei.php
@@ -0,0 +1,76 @@
+				<div class="dropDownColumn">
+					<?php 
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h2>
+							<?php	
+						} else {
+							?>
+							<h3><img src="https://springcreek.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Title I Information</h3>
+							<?php
+						}	
+					?>
+						<ul>
+							<li class="int"><a href="https://timpanogos.provo.edu/title-i/what-is-title-i/">What is Title I?</a></li>
+							<li class="pdf"><a href="https://timpanogos.provo.edu/wp-content/uploads/2019/12/19-20-Needs-Assessment.pdf">Needs Assessment</a></li>
+							<li class="pdf"><a href="https://timpanogos.provo.edu/wp-content/uploads/2020/12/Timpanogos-Elementary-School-School-Improvement-Plan-2020-21.pdf">School Improvement Plan</a></li>
+						</ul>
+						<p>Title I Coordinator: Kate Pace (KateP@provo.edu)</p>
+				</div>
+				<div class="dropDownColumn">
+					<?php 
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://timpanogos.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent-School Learning Compact</h2>
+							<?php	
+						} else {
+							?>
+							<h3><img src="https://timpanogos.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent-School Learning Compact</h3>
+							<?php
+						}	
+					?>
+					
+						<ul>
+							<li class="pdf"><a href="https://timpanogos.provo.edu/wp-content/uploads/2021/09/TIMP_ParentCompact_fy22_Eng.pdf">Parent-School Learning Compact (English)</a></li>
+							<li class="pdf"><a href="https://timpanogos.provo.edu/wp-content/uploads/2021/09/TIMP_ParentCompact_fy22_Spa.pdf">Parent-School Learning Compact (Spanish)</a></li>
+						</ul>
+					<?php 
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://timpanogos.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent Engagement Policy</h2>
+							<?php	
+						} else {
+							?>
+							<h3><img src="https://timpanogos.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Parent Engagement Policy</h3>
+							<?php
+						}	
+					?>
+					
+						<ul>
+							<li class="pdf"><a href="https://timpanogos.provo.edu/wp-content/uploads/2021/09/TIMP_ParentEngagementPolicy_fy22_Eng.pdf">Parent Engagement Policy (English)</a></li>
+							<li class="pdf"><a href="https://timpanogos.provo.edu/wp-content/uploads/2021/09/TIMP_ParentEngagementPolicy_fy22_Spa.pdf">Parent Engagement Policy (Spanish)</a></li>
+						</ul>
+				</div>
+				<div class="dropDownColumn">
+					<?php 
+						//checks if this template section is loading into the menu or into the sidebar/content area and assigns the proper heading
+						if(is_page( 'title-i' ) || in_array( 623, get_post_ancestors($post))){
+							?>
+							<h2><img src="https://amelia.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h2>
+							<?php	
+						} else {
+							?>
+							<h3><img src="https://amelia.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h3>
+							<?php
+						}	
+					?>
+						<ul>
+							<li class="pdf"><a href="https://timpanogos.provo.edu/wp-content/uploads/2019/01/01102019-titlei-parent-notification-compliance.pdf">Parent Notification Compliance</a></li>
+							<li class="ext"><a href="https://utahschoolgrades.schools.utah.gov/Home/SchoolOverAllPerformance?SchoolID=1234&DistrictID=1222&SchoolNbr=128&SchoolLevel=K8&IsSplitSchool=0">School Report Card</a></li>
+							<li class="pdf"><a href="https://timpanogos.provo.edu/wp-content/uploads/2020/03/Timp-Goals-at-a-Glance.pdf">Goals at a Glance</a></li>
+							<li class="ext"><a href="https://provo.edu/title-i/">District Title I</a></li>
+						</ul>
+				</div>
